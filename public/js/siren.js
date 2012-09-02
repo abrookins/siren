eb59eedeac73c4f7ec34ca4e5a927e92e4eae8c2
Andrew Brookins <a.m.brookins@gmail.com>
2012-09-01 20:52:51 -0700

    Add twitter bootstrap and a basic web app to view crimes for user location

---
+++ public/js/siren.js
@@ -0,0 +1,44 @@
+(function() {
+
+    function getMap(coords) {
+        var width = $('.container').width();
+        var latlon=coords.latitude+","+coords.longitude;
+        var imgUrl="http://maps.googleapis.com/maps/api/staticmap?center="
+            +latlon+"&zoom=14&size="+width+"x200&sensor=false&markers=color:blue%7C"
+            +latlon+'&scale=2';
+        console.log(imgUrl);
+
+        $('#map').html("<img src='"+imgUrl+"' />");
+    }
+
+    function getCrimes(loc) {
+        $.get('http://localhost:5000/crime_stats?point=' + loc.coords.latitude + ',' + loc.coords.longitude, function(data) {
+
+            $.each(data.result.stats, function () {
+                var crime = this[0];
+                var numCrimes = this[1];
+                var colorClass = "success";
+
+                if (numCrimes > 25 && numCrimes <= 100) {
+                    colorClass = 'info';
+                } else if (numCrimes > 100) {
+                    colorClass = 'error';
+                }
+
+                var tr = $('<tr/>').appendTo($('#crimes')).addClass(colorClass);
+
+                $('<td/>').text(numCrimes).appendTo(tr);
+                $('<td/>').text(crime).appendTo(tr);
+                $('<td/>').appendTo(tr);
+            });
+
+            getMap(loc.coords);
+        });
+    }
+
+    function reportError(err) {
+        console.log(err);
+    }
+
+    navigator.geolocation.getCurrentPosition(getCrimes, reportError);
+})();
