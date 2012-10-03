c464a5059020d369dced01bd0c5ce296b998f00c
Andrew Brookins <a.m.brookins@gmail.com>
2012-10-02 23:00:36 -0700

    Add Flask-Cache to requirements.txt

--- fabfile.py
+++ fabfile.py
@@ -6,9 +6,14 @@ DEPLOY_DIR = '/home/andrew/apps/siren'
 
 @api.task
 @api.hosts('andrew@andrewbrookins.com')
-def deploy():
+def deploy(install_reqs=False):
     api.local('git push origin master')
 
     with api.cd(DEPLOY_DIR):
        api.run('git pull origin master')
+
+       if install_reqs:
+           api.run('export WORKON_HOME=~/envs && source virtualenvwrapper.sh '
+                   '&& workon siren && pip install -r requirements.txt')
+
        api.sudo('supervisorctl restart siren')
