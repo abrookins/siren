930770c1aade955508432bb0a54f63a52c4074e3
Andrew Brookins <a.m.brookins@gmail.com>
2013-04-30 00:06:01 -0700

    Add a locust file for quick-and-dirty load testing.

--- bin/ping.py
+++ bin/ping.py
@@ -3,6 +3,7 @@
 import urllib2
 
 # Ping the site every 10 minutes, to make sure Heroku doesn't spin it down.
-resp = urllib2.urlopen('http://pdxcrime.herokuapp.com')
+resp = urllib2.urlopen(
+    'http://pdxcrime.herokuapp.com/crime/stats/45.6159782376,-122.690757299')
 
 print resp.read()
---
+++ locustfile.py
@@ -0,0 +1,16 @@
+from locust import Locust, TaskSet
+
+
+url = '/crime/stats/%s'
+
+with open('portland_coordinates.txt') as f:
+    coords = [l for l in f.read().split('\n') if l]
+    tasks = {lambda l: l.client.get(url % coord) for coord in coords}
+
+
+class ApiTasks(TaskSet):
+    tasks = tasks
+
+
+class ApiLocust(Locust):
+    task_set = ApiTasks
